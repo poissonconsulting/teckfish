fa4efcfbdb66255d40d89389dd23c56ed12a4ee9
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-11 15:52:08 -0700

    adding tests for calculate gsdd

--- tests/testthat/test-calculate_gsdd.R
+++ tests/testthat/test-calculate_gsdd.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("output is a numeric value", {
+  day <- 1:365
+  temperature <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
+  gsdd <- calculate_gsdd(x = temperature, k = 7, starttemp = 5, endtemp =4)
+  expect_true(is.numeric(gsdd))
 })
