8ec08a969c8cba0dccd78a52fa2191b9b8b8526e
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-11 15:40:45 -0700

    changes to calculate gsdd function, add example code

--- R/calculate_gsdd.R
+++ R/calculate_gsdd.R
@@ -9,31 +9,24 @@
 #' @export
 #'
 #' @examples
+#' day <- 1:365
+#' temperature <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
+#' calculate_gsdd(x = temperature, k = 7, starttemp = 5, endtemp =4)
+#' 
 calculate_gsdd <- function(x, k, starttemp, endtemp) {
   # calculate rolling average
-  rollingmean <- zoo::rollmean(x=x, k=k)
+  x <- zoo::rollmean(x=x, k=k)
   
-  # remove days that are above startttemp and below endtemp
+  # remove days that are above start temp and below end temp
   start <- match(TRUE, x > starttemp)
-  end <- match(TRUE, x[start:length(x)] < endtemp)
-  temperature_filtered <- x[start:end]
+  x <- x[start:length(x)]
   
-  # Subtract base temp (starttemp) and sum for each season. 
-  gsdd <- sum(temperature - starttemp)
+  end <- match(TRUE, x < endtemp)
+  temperature_filtered <- x[1:end]
+  
+  # Subtract base temp (start temp) and sum for each season. 
+  cumulative_gsdd <- sum(temperature_filtered - starttemp)
+  return(cumulative_gsdd)
 }
-
-
-# Simulate annual temp data for dev - will remove
-# day <- 1:365
-# temperature <- -15 * cos((2*pi / 365) * (day-60)) + rnorm(365, mean = 10, sd = 1)
-# 
-# plot(day, temperature, type = "l",
-#      xlab = "Day",
-#      ylab = "Temperature (°C)")
-# x <- temperature
-# k <- 7
-
-starttemp <- 5
-endtemp <- 4
 
 
