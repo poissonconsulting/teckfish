13dd7e762aef7b5bf229109a3a6b896626530ab8
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-13 13:14:34 -0700

    change to function to make sure growing season is defined when temps have risen for a given consecutive number of days.

--- R/calculate_gsdd.R
+++ R/calculate_gsdd.R
@@ -1,20 +1,20 @@
-#' calculate growing season degree days.
+#' calculate growing season degree days for a vector of temperature values. 
 #'
 #' @param x vector of numeric temperature data. 
 #' @param rollmean_units positive whole number, width of rolling mean window. 
 #' @param starttemp single numeric value, threshold rolling average temperature to define start of the growing season
 #' @param endtemp single numeric value, weekly temperature rolling average threshold to define end of growing season. 
-#' 
+#' @param n_consecutive single positive whole number. Number of consecutive rolling average days above and below cut off temperatures to begin gsdd calculations
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' day <- 1:365
+#' set.seed(13)
 #' temperature <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
-#' calculate_gsdd(x = temperature, rollmean_units = 7, start_temp = 5, end_temp = 4)
+#' calculate_gsdd(x = temperature, rollmean_units = 7, start_temp = 5, end_temp = 4, n_consecutive = 5)
 #' 
-calculate_gsdd <- function(x, rollmean_units, start_temp = 5, end_temp = 4) {
+calculate_gsdd <- function(x, rollmean_units = 7, start_temp = 5, end_temp = 4, n_consecutive = 5) {
   chk_vector(x)
   chk_all(temperature, chk_number)
   chk_length(start_temp, length = 1)
@@ -22,17 +22,22 @@ calculate_gsdd <- function(x, rollmean_units, start_temp = 5, end_temp = 4) {
   chk_count(rollmean_units)
   chk_numeric(start_temp)
   chk_numeric(end_temp)
-
-  x <- zoo::rollmean(x=x, rollmean_units=rollmean_units)
+  chk_count(n_consecutive)
   
-  start <- match(TRUE, x > start_temp)
-  x <- x[(start-3):length(x)]
+  x <- zoo::rollmean(x=x, k=rollmean_units)
   
-  end <- match(TRUE, x < end_temp)
-  temperature_filtered <- x[1:(end+3)]
-
-  cumulative_gsdd <- sum(temperature_filtered)
+  indices <- which(x > start_temp)
+  matches <- zoo::rollapply(indices, width = n_consecutive, FUN = function(x) all(diff(x) == 1))
+  first_match_index <- min(indices[matches])
+  
+  x <- x[(first_match_index-3):length(x)]
+  
+  indices <- which(x < end_temp)
+  matches <- zoo::rollapply(indices, width = n_consecutive, FUN = function(x) all(diff(x) == 1))
+  end_match_index <- min(indices[matches])
+  
+  x <- x[1:(end_match_index+3)]
+ 
+  cumulative_gsdd <- sum(x)
   return(cumulative_gsdd)
 }
-
-
