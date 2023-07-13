df8583f562aed95a40328fc6a042d4e6c7cf8fe8
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-13 13:53:11 -0700

    minor changes

--- tests/testthat/test-calculate_gsdd.R
+++ tests/testthat/test-calculate_gsdd.R
@@ -1,7 +1,7 @@
-test_that("output is a numeric value", {
+testthat::test_that("output is a numeric value", {
   set.seed(13)
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
   output <- calculate_gsdd(x, rollmean_units = 7, start_temp = 5, end_temp = 4, n_consecutive = 5)
-  expect_equal(is.numeric(output), TRUE)
+  testthat::expect_equal(is.numeric(output), TRUE)
 })
