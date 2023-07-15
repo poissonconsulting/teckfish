928baaa45aa6f8e08b1a12cc26d3a4f4fffa5ac3
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-14 17:30:03 -0700

    function changed to error if start temp not reached and to give a warning if end temp not reached

--- R/calculate_gsdd.R
+++ R/calculate_gsdd.R
@@ -24,22 +24,30 @@ calculate_gsdd <- function(x, rollmean_units = 7, start_temp = 5, end_temp = 4, 
   chk::chk_numeric(start_temp)
   chk::chk_numeric(end_temp)
   chk::chk_count(n_consecutive)
+  if(max(x) <= start_temp){
+    stop("Error: start temp never reached in vector")
+  }
   
   rollmean <- zoo::rollmean(x=x, k=rollmean_units)
   
   indices <- which(rollmean > start_temp)
   matches <- zoo::rollapply(indices, width = n_consecutive, FUN = function(rollmean) all(diff(rollmean) == 1))
   first_match_index <- min(indices[matches])
-  
   rollmean <- rollmean[(first_match_index-3):length(rollmean)]
   
-  
   indices <- which(rollmean < end_temp)
+  
+  if (length(indices) == 0) {
+    indices <- (length(rollmean) - 3)
+    warning("end_temp never reached, gsdd calculated for remainder of values")
+    x <- x[(first_match_index-3):length(x)]
+  } else {
+  indices <- indices
   matches <- zoo::rollapply(indices, width = n_consecutive, FUN = function(rollmean) all(diff(rollmean) == 1))
   end_match_index <- min(indices[matches])
-  
   x <- x[(first_match_index-3):(end_match_index+3)]
- 
+  }
+  
   cumulative_gsdd <- sum(x)
   return(cumulative_gsdd)
 }
