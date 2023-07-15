4da0d2de06899bdc6cc016f0d8e2fbaa8affb1db
Sarah Lyons <sarah@poissonconsulting.ca>
2023-07-14 17:59:29 -0700

    added more tests to test file

--- tests/testthat/test-calculate_gsdd.R
+++ tests/testthat/test-calculate_gsdd.R
@@ -3,5 +3,27 @@ testthat::test_that("output is a numeric value", {
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
   output <- calculate_gsdd(x, rollmean_units = 7, start_temp = 5, end_temp = 4, n_consecutive = 5)
+  testthat::expect_true(is.numeric(output))
+})
+
+testthat::test_that("start temp must be greater than or equal to end temp", {
+  set.seed(13)
+  day <- 1:365
+  x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
+  output <- calculate_gsdd(x, rollmean_units = 7, start_temp = 5, end_temp = 40, n_consecutive = 5)
   testthat::expect_equal(is.numeric(output), TRUE)
+})
+
+testthat::test_that("if max temp in vector is lower than start_temp the function will error", {
+  set.seed(13)
+  day <- 1:365
+  x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
+  testthat::expect_error(calculate_gsdd(x = x, rollmean_units = 7, start_temp = 50, end_temp = 4, n_consecutive = 5))
+})
+
+testthat::test_that("if end_temp is not reached, gsdd calculated to end of vector and warning is shown", {
+  set.seed(13)
+  day <- 1:365
+  x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
+  testthat::expect_warning(calculate_gsdd(x, rollmean_units = 7, start_temp = 5, end_temp = -40, n_consecutive = 5))
 })
