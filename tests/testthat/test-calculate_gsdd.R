dec54d5f5455582a6bbc9aff8af22536e6a24be4
Seb Dalgarno <seb@poissonconsulting.ca>
2023-07-27 16:24:54 -0700

    use expect_chk_error to be more precise (and avoid some other unexpected error causing test to pass)

--- tests/testthat/test-calculate_gsdd.R
+++ tests/testthat/test-calculate_gsdd.R
@@ -10,14 +10,14 @@ test_that("vector must be longer than window_width", {
   set.seed(13)
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
-  expect_error(calculate_gsdd(x, window_width = 369, start_temp = 5, end_temp = 4, n_consecutive = 5))
+  expect_chk_error(calculate_gsdd(x, window_width = 369, start_temp = 5, end_temp = 4, n_consecutive = 5))
 })
 
 test_that("vector must be longer than n_consecutive", {
   set.seed(13)
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
-  expect_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 4, n_consecutive = 369))
+  expect_chk_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 4, n_consecutive = 369))
 })
 
 test_that("vector must not contain NA values", {
@@ -26,21 +26,21 @@ test_that("vector must not contain NA values", {
   x <- -15 * cos((2 * pi / 365) * (day - 10)) + rnorm(365, mean = 10, sd = 0.5)
   random_indices <- sample(1:length(x), 40)
   x[random_indices] <- NA
-  expect_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 4, n_consecutive = 5))
+  expect_chk_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 4, n_consecutive = 5))
 })
 
 test_that("start temp must be greater than or equal to end temp", {
   set.seed(13)
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
-  expect_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 40, n_consecutive = 5))
+  expect_chk_error(calculate_gsdd(x, window_width = 7, start_temp = 5, end_temp = 40, n_consecutive = 5))
 })
 
 test_that("if max temp in vector is lower than start_temp the function will error", {
   set.seed(13)
   day <- 1:365
   x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
-  expect_error(calculate_gsdd(x = x, window_width = 7, start_temp = 50, end_temp = 4, n_consecutive = 5))
+  expect_chk_error(calculate_gsdd(x = x, window_width = 7, start_temp = 50, end_temp = 4, n_consecutive = 5))
 })
 
 test_that("if end_temp is not reached, gsdd calculated to end of vector and warning is shown", {
