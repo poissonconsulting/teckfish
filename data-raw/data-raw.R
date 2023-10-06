aad50b29e7b0db3ea23d5fb1624afa97377351ba
Sarah Lyons <sarah@poissonconsulting.ca>
2023-10-06 12:30:48 -0700

    added apache license text in data-raw.R

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,7 +1,20 @@
-## code to prepare `DATASET` dataset goes here
+# Copyright 2023 Province of Alberta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set.seed(13)
 day <- 1:365
 x <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
 x[x < 0] <- 0
-usethis::use_data(DATASET, overwrite = TRUE)
-use_data()
+
+usethis::use_data(simulated_data, overwrite = TRUE)
