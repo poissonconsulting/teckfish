624ecd7dd6c49d5676ef446e4037a1ddb8b48968
Sarah Lyons <sarah@poissonconsulting.ca>
2023-10-06 13:24:37 -0700

    fix data-raw simulated data definition

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -17,4 +17,8 @@ day <- 1:365
 simulated_data <- -15 * cos((2*pi / 365) * (day-10)) + rnorm(365, mean = 10, sd = .5)
 simulated_data[simulated_data < 0] <- 0
 
+simulated_data <- tibble::tibble(
+  date = seq.Date(from = as.Date("2019-01-01"), to = as.Date("2019-12-31"), by = "day"),
+  synthetic = simulated_data
+)
 usethis::use_data(simulated_data, overwrite = TRUE)
