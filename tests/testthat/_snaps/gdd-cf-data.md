a08372a43cf56545944da3c1528d567ce2d194bc
Joe Thorley <joe@poissonconsulting.ca>
2024-03-23 19:01:33 -0700

    fixing up gdd_cf_data

--- tests/testthat/_snaps/gdd-cf-data.md
+++ tests/testthat/_snaps/gdd-cf-data.md
@@ -20,6 +20,47 @@
         <int> <date>      <date>     <dbl>
       1  2019 1972-03-16  1972-12-13 3617.
 
+# gdd_cf_data shifts by 10
+
+    Code
+      gdd
+    Output
+      # A tibble: 1 x 4
+      # Groups:   year [1]
+         year start_dayte end_dayte   gsdd
+        <int> <date>      <date>     <dbl>
+      1  2019 1972-03-09  1972-11-26 3605.
+
+# gdd_cf_data preserves if shift start date
+
+    Code
+      gdd
+    Output
+      # A tibble: 1 x 4
+      # Groups:   year [1]
+         year start_dayte end_dayte   gsdd
+        <int> <date>      <date>     <dbl>
+      1  2019 1972-03-19  1972-11-15 3605.
+
+---
+
+    Code
+      gdd
+    Output
+      # A tibble: 1 x 4
+      # Groups:   year [1]
+         year start_dayte end_dayte   gsdd
+        <int> <date>      <date>     <dbl>
+      1  2019 1971-03-20  1971-09-30 3605.
+
+# gdd_cf_data one message
+
+    Code
+      gdd
+    Output
+      # A tibble: 0 x 4
+      # i 4 variables: year <int>, start_dayte <date>, end_dayte <date>, gsdd <dbl>
+
 # gdd_cf_data works shortened
 
     Code
--- tests/testthat/test-gdd-cf-data.R
+++ tests/testthat/test-gdd-cf-data.R
@@ -7,6 +7,46 @@ test_that("gdd_cf_data works", {
   })
 })
 
+test_that("gdd_cf_data shifts by 10", {
+  data <- simulated_data
+  data$temperature <- data$synthetic
+  data <- data[-(1:10),]
+  gdd <- gdd_cf_data(data)
+  expect_snapshot({
+    gdd
+  })
+})
+
+test_that("gdd_cf_data preserves if shift start date", {
+  data <- simulated_data
+  data$temperature <- data$synthetic
+  data <- data[-(1:10),]
+  gdd <- gdd_cf_data(data, start_date = as.Date("2019-02-01"))
+  expect_snapshot({
+    gdd
+  })
+})
+
+test_that("gdd_cf_data preserves if shift start date", {
+  data <- simulated_data
+  data$temperature <- data$synthetic
+  gdd <- gdd_cf_data(data, start_date = as.Date("2019-03-20"), ignore_truncation = TRUE)
+  expect_snapshot({
+    gdd
+  })
+})
+
+test_that("gdd_cf_data one message", {
+  data <- simulated_data
+  data$temperature <- data$synthetic
+  data <- data[-(1:100),]
+  expect_message(gdd <- gdd_cf_data(data), "The growing season is truncated at the start of the sequence.")
+  expect_snapshot({
+    gdd
+  })
+})
+
+
 test_that("gdd_cf_data works", {
   data <- simulated_data
   data$temperature <- data$synthetic
