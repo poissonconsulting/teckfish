bea311b665fb64a82444af00b02d1d14b5d6de0e
Joe Thorley <joe@poissonconsulting.ca>
2024-03-24 19:27:22 -0700

    - Soft-deprecated `gsdd_cf()` for `gsdd_vctr()`.

--- R/gsdd-vctr.R
+++ R/gsdd-vctr.R
@@ -6,18 +6,18 @@
 #' @export
 #'
 #' @examples
-#' gsdd_cf(c(rep(1, 10), rep(10, 20), rep(1, 200)))
-#' gsdd_cf(teckfish::simulated_data$synthetic)
-gsdd_cf <- function(
+#' gsdd_vctr(c(rep(1, 10), rep(10, 20), rep(1, 200)))
+#' gsdd_vctr(teckfish::simulated_data$synthetic)
+gsdd_vctr <- function(
     x,
     ignore_truncation = FALSE,
-    min_length = 184,
+    min_length = 274,
     msgs = TRUE
 ) {
-  lifecycle::deprecate_soft("0.1.2", "gsdd_cf()", with = "gsdd_vctr()")
-  gsdd_vctr(
+  gsdd::gsdd_vctr(
     x, 
     ignore_truncation = ignore_truncation, 
     min_length = min_length, 
+    pick = "longest",
     msgs = msgs)
 }
